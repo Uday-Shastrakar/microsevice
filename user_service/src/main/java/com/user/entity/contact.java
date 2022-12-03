b67fae0a4e19f25d1f3eeeb19be65808ebf5041e
Uday Shastrakar <uday.shastrakar@gmail.com>
2022-12-03 19:20:58 +0530

    initial commit

--- user_service/src/main/java/com/user/entity/contact.java
+++ user_service/src/main/java/com/user/entity/contact.java
@@ -19,5 +19,35 @@ public class contact {
 
     }
 
-    
+    public Long getcId() {3
+        return cId;
+    }
+
+    public void setcId(Long cId) {
+        this.cId = cId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
